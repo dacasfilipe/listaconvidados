3523ddadbb2027169efc3f9bee8246fe3cf5f6a2
dacasfilipe <filiperdacas@gmail.com>
2024-04-19 19:16:59 -0300

    atualizado ConvidadosController.java

--- src/main/java/com/convidados/controllers/ConvidadosController.java
+++ src/main/java/com/convidados/controllers/ConvidadosController.java
@@ -1,12 +1,23 @@
 package com.convidados.controllers;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class ConvidadosController {
+public class ConvidadosController {.
+    //localhost:8080/convidados/listar
     @GetMapping("/convidados")
-    public String listar(){
-        return "Lista de Convidados.";
+    public ModelAndView listar(){
+    //cria o objeto mv e insere uma lista de convidados
+    ModelAndView mv = new ModelAndView("ListaConvidados");
+    //envia um objeto convidado para o mv
+    mv.addObject(new Convidado());
+    //envia uma lista "convidados" da tabela convidados
+    mv.addObject("convidados",
+            convidadosRepository.findAll());
+    return mv;
     }
 }
